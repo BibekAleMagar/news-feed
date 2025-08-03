81436b09db4c25328c93ad457527aef4e0c80e68
Bibek Ale Magar <magarbibek71@gmail.com>
2025-08-03 11:14:55 +0545

    :memo: Fxied Router

--- news/src/main.tsx
+++ news/src/main.tsx
@@ -2,20 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 import { queryClient } from './lib/react-query.ts'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/news-feed'>
+    <HashRouter basename='/news-feed'>
       <QueryClientProvider client={queryClient}>
         <App />
         <Toaster position='top-center' />
         <ReactQueryDevtools initialIsOpen={false}  />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
